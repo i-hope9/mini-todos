35786d6fb7b1148d362d0f189c379b1a2d4cb719
Eeejeong Kim <ennea9on@gmail.com>
2022-12-12 14:00:50 +0900

    feat: add filtering function

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: 1, status: "active", content: "리액트 강의 듣기" },
     { id: 2, status: "active", content: "운동하기" },
@@ -13,7 +13,6 @@ export default function TodoList() {
     setTodos(prev => [...prev, todo]);
   }
 
-
   const handleDelete = (deleted) => {
     setTodos(prev => prev.filter(todo => todo.id !== deleted.id));
   }
@@ -22,11 +21,13 @@ export default function TodoList() {
     setTodos(prev => prev.map(todo => todo.id === updated.id ? updated : todo));
   }
 
+  const filteredTodos = getFilteredItem(todos, filter);
+
   return (
     <>
     <section>
       <ul>
-        {todos.map((todo) => (
+        {filteredTodos.map((todo) => (
           <Todo key={todo.id} todo={todo} onUpdate={handleUpdate} onDelete={handleDelete}/>
           // <li key={todo.id}><Checkbox onChecked={handleChecked} todo={todo}/>{todo.content}<button onClick={handleDelete} value={todo.id}>삭제</button></li>
         ))}
@@ -36,4 +37,11 @@ export default function TodoList() {
     <AddTodo onAdd={handleAdd}></AddTodo> {/* 이벤트가 발생했을 때 나에게 알려줄 콜백 함수를 지정 */}
     </>
   );
+}
+
+const getFilteredItem = (todos, filter) => {
+  if (filter === 'all') {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
